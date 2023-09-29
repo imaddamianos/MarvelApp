6e918f2f658d0c57691657b1c55699334f0d8cc0
iMad <damianos.imad@gmail.com>
2023-09-29 14:09:03 +0300

    creating models for each category

---
+++ MarvelHeros/Model/ComicsSummary.swift
@@ -0,0 +1,37 @@
+//
+//  ComicsSummary.swift
+//  MarvelHeros
+//
+//  Created by iMad on 29/09/2023.
+//
+
+import Foundation
+
+struct ComicsSummary {
+    
+    let resourceURI: String?
+    let name: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case resourceURI
+        case name
+    }
+}
+
+extension ComicsSummary: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.resourceURI = try container.decodeIfPresent(String.self, forKey: .resourceURI)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)
+    }
+}
+
+extension ComicsSummary: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(resourceURI, forKey: .resourceURI)
+        try container.encodeIfPresent(name, forKey: .name)
+    }
+}
+
+
