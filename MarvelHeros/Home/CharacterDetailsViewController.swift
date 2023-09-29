933dd6571184cd89c4c0514b93eff734d97e51b6
iMad <damianos.imad@gmail.com>
2023-09-29 23:35:29 +0300

    creating series collection view

--- MarvelHeros/Home/CharacterDetailsViewController.swift
+++ MarvelHeros/Home/CharacterDetailsViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class CharacterDetailsViewController: UIViewController {
+    @IBOutlet weak var characterImg: UIImageView!
+    @IBOutlet weak var characterName: UILabel!
+    @IBOutlet weak var seriesCollView: UICollectionView!
     let imageDownloader = ImageDownloader()
     var character: Character?
     
@@ -19,23 +22,57 @@ class CharacterDetailsViewController: UIViewController {
     }
     
     func setupView(){
-//        GFunction.shared.addLoader("Loading details")
-               if let character = character {
-//                   nameLabel.text = character.name
-//                   descriptionLabel.text = character.description
-                   imageDownloader.downloadImage(from: (character.thumbnail?.url)!) { image in
-                       if let image = image {
-                           // Handle the downloaded image
-                           DispatchQueue.main.async {
-//                               GFunction.shared.removeLoader()
-//                               self.thumbnailImageView.image = image
-                           }
-                       } else {
-                           // Handle the case where image download failed
-                           print("Failed to download image.")
-                       }
-                   }
-               }
-    }
+        seriesCollView.delegate = self
+        seriesCollView.dataSource = self
+        seriesCollView.register(SeriesCollectionViewCell.nib, forCellWithReuseIdentifier: SeriesCollectionViewCell.identifier)
+        if let character = character {
+            if let seriesItems = character.series?.items {
+                // Limit seriesArray to the first 3 items
+                seriesArray = Array(seriesItems.prefix(3))
+                self.seriesCollView.reloadData()
+                
+                imageDownloader.downloadImage(from: (character.thumbnail?.url)!) { image in
+                    if let image = image {
+                        // Handle the downloaded image
+                        DispatchQueue.main.async {
+                            self.characterImg.image = image
+                            self.characterName.text = character.name
+                        }
+                    } else {
+                        // Handle the case where image download failed
+                        print("Failed to download image.")
+                    }
+                }
+            }
+        }
 
+        
+            }
+        }
+
+// MARK: - UICollectionView
+
+extension CharacterDetailsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return seriesArray.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 60)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        if collectionView == seriesCollView{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SeriesCollectionViewCell.identifier, for: indexPath) as! SeriesCollectionViewCell
+            let series = seriesArray[indexPath.row]
+        cell.seriesName.text = series.name
+            return cell
+//        }
+//        return UICollectionViewCell()
+        
+    }
+    
+    
 }
